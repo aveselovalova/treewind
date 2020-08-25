c589df3c12a98511a3c3a9e8ad5dab36cd0371e9
Aleksandra Veselova <aveselovalova@gmail.com>
2020-08-25 23:36:18 +0300

    removed unused functions

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -29,24 +29,6 @@ const generateFibonacciLongitudes = (longitude: number, windPower = 0): number[]
 	return longitudes;
 };
 
-const generateFunction = (windLongitudes: number[], latitude, longitude, increment: number): number[][] => {
-	const l = [[latitude, longitude]];
-	const r: number[][] = [];
-	const isLastPoint = windLongitudes.length - 1;
-	for (let i = 0; i < windLongitudes.length; i++) {
-		const currentLat = l[i][0];
-		const currentLon = windLongitudes[i];
-		l.push([i === isLastPoint ? currentLat : currentLat + increment, currentLon]);
-
-		let item = latitude - LATITUDE_WIND_INCREMENT * (i + 1);
-		if (i === isLastPoint) {
-			item = r[r.length - 1][0];
-		}
-		r.push([item, currentLon]);
-	}
-	return [...l, ...r.reverse()];
-};
-
 const generateFunctionGeoJson = (windLongitudes: number[], latitude, longitude, increment: number): number[][] => {
 	const l = [[longitude, latitude]];
 	const r: number[][] = [];
@@ -67,24 +49,11 @@ const generateFunctionGeoJson = (windLongitudes: number[], latitude, longitude, 
 
 export const toRadians = degree => degree * (Math.PI / 180);
 
-function rotate(treeLon, treeLat, x, y, radians) {
-	const cos = Math.cos(radians);
-	const sin = Math.sin(radians);
-	return [cos * (y - treeLat) - sin * (x - treeLon) + treeLat, cos * (x - treeLon) + sin * (y - treeLat) + treeLon];
-}
-
 function rotateGeoJson(treeLon, treeLat, x, y, radians) {
 	const cos = Math.cos(radians);
 	const sin = Math.sin(radians);
 	return [cos * (x - treeLon) + sin * (y - treeLat) + treeLon, cos * (y - treeLat) - sin * (x - treeLon) + treeLat];
 }
-
-export const getWindLayerCoordinates = (longitude: number, latitude: number, windPower = 0, radians?) => {
-	const longitudes = generateFibonacciLongitudes(longitude, windPower); // web 0 === 90 of azimuth (->)
-	const generated = generateFunction(longitudes, latitude, longitude, LATITUDE_WIND_INCREMENT);
-	const rotated = generated.map(a => rotate(longitude, latitude, a[1], a[0], radians)); // angle has +90 degrees offset
-	return [...rotated, [latitude, longitude]];
-};
 
 export const getWindLayerCoordinatesGeoJson = (longitude: number, latitude: number, windPower = 0, radians?) => {
 	const longitudes = generateFibonacciLongitudes(longitude, windPower); // web 0 === 90 of azimuth (->)
